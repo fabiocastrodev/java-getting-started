d8825d85fefc687086a112d207fe9b2ecfa678c6
fabiocastrodev <fabiocastrodev@gmail.com>
2018-12-05 18:39:44 -0500

    corrige assert

--- src/test/java/BhaskaraTest.java
+++ src/test/java/BhaskaraTest.java
@@ -6,21 +6,21 @@ public class BhaskaraTest {
   @Test
   public void Teste1() {
       Bhaskara bhask = new Bhaskara(1.0, -5.0, 6.0);
-      assertEquals(3.0, bhask.getX1());
-      assertEquals(2.0, bhask.getX2());
+      assertEquals(3.0, bhask.getX1(), 0.1);
+      assertEquals(2.0, bhask.getX2(), 0.1);
   }
   
   @Test
   public void Teste2() {
       Bhaskara bhask = new Bhaskara(1.0, 8.0, -9.0);
-      assertEquals(1.0, bhask.getX1());
-      assertEquals(-9.0, bhask.getX2());
+      assertEquals(1.0, bhask.getX1(), 0.1);
+      assertEquals(-9.0, bhask.getX2(), 0.1);
   }
   
   @Test
   public void Teste3() {
       Bhaskara bhask = new Bhaskara(1.0, 3.0, -4.0);
-      assertEquals(1.0, bhask.getX1());
-      assertEquals(-4.0, bhask.getX2());
+      assertEquals(1.0, bhask.getX1(), 0.1);
+      assertEquals(-4.0, bhask.getX2()), 0.1;
   }
 }
